39ea66383296baa8dde9e0a8f7a7bf01d0ce20a6
kiswaafaq <kiswaafaque14@gmail.com>
2024-02-23 21:42:28 -0800

    First Commit

---
+++ 6. Stripping Names.ts
@@ -0,0 +1,2 @@
+const personName: string = '/t/t/nKiswa Afaq/t/t/n';
+console.log(personName);
---
+++ Stripping_Names.js
@@ -0,0 +1,7 @@
+"use strict";
+
+
+const personName = '/t/t/nKiswa Afaq/t/t/n';
+
+
+console.log(personName);
